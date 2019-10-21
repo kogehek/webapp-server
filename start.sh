5331a39f1d1acf952cea4d7d50f614c7d4f9e7fa
unknown <kogehek>
2019-10-21 23:02:32 +0300

    docker update install composer

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 ROOT_PATH=$(pwd)
 
-WIN_PREF=""
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    WIN_PREF="winpty"
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    WIN_PREF="winpty"
-fi
-
 echo 'Use default settings (Y/n)?'
 read answer
 if [ "$answer" != "${answer#[Nn]}" ] ;then
@@ -30,7 +23,9 @@ fi
 
 
 docker-compose up --build -d
-$WIN_PREF docker-compose exec php-fpm php artisan key:generate
-$WIN_PREF docker-compose exec php-fpm php artisan clear-compiled
-$WIN_PREF docker-compose exec php-fpm php artisan optimize
+docker-compose exec --user devuser php-fpm composer install
+docker-compose exec --user devuser php-fpm php artisan key:generate
+docker-compose exec php-fpm php artisan clear-compiled
+docker-compose exec php-fpm php artisan optimize
+docker-compose exec --user devuser php-fpm php artisan config:cache
 
