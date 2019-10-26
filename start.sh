a17b411a0785622cab0d3faa3292cb1300ceb565
unknown <kogehek>
2019-10-26 18:38:55 +0300

    docker fix database

--- start.sh
+++ start.sh
@@ -14,5 +14,7 @@ docker-compose exec --user devuser php-fpm php artisan key:generate
 docker-compose exec php-fpm php artisan clear-compiled
 docker-compose exec php-fpm php artisan optimize
 docker-compose exec --user devuser php-fpm php artisan config:cache
+docker-compose exec --user devuser php-fpm php artisan route:clear
+docker-compose exec --user devuser php-fpm php artisan migrate
 
 echo "http://127.0.0.1"
