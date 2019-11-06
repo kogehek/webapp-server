d3ea5f46bb1313fda7425cbfb11126b2a2875405
unknown <kogehek>
2019-11-06 23:02:16 +0300

    fix key:generate in docker

--- start.sh
+++ start.sh
@@ -11,10 +11,12 @@ cp .env.example .env
 docker-compose up --build -d
 docker-compose exec --user devuser php-fpm composer install
 docker-compose exec --user devuser php-fpm php artisan key:generate
-docker-compose exec php-fpm php artisan clear-compiled
-docker-compose exec php-fpm php artisan optimize
+docker-compose exec --user devuser php-fpm php artisan clear-compiled
+docker-compose exec --user devuser php-fpm php artisan optimize
 docker-compose exec --user devuser php-fpm php artisan config:cache
 docker-compose exec --user devuser php-fpm php artisan route:clear
 docker-compose exec --user devuser php-fpm php artisan migrate
+
+# docker-compose exec --user devuser php-fpm php artisan config:cache // Production
 
 echo "http://127.0.0.1"
